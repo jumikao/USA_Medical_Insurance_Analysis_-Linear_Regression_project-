84089cf138d73119cca65bfac43adcd5ba858ae6
jumikao <mezykowska.ma@gmail.com>
2022-06-21 19:35:10 +0200

    zmiany

--- get_data.py
+++
@@ -1,8 +0,0 @@
-import pandas as pd
-
-data_df = pd.read_csv(r'https://raw.githubusercontent.com/Kamil128/ProjektPraktycznyRegresja/main/data/medical_cost/medical_cost.csv',
-                 )
-
-print(data_df.head())
-print("........")
-print(data_df.columns)
