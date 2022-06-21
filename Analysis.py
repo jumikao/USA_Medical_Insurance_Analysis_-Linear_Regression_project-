39bf0513d98b335d6fca1e3cefd18062bb417658
jumikao <mezykowska.ma@gmail.com>
2022-06-21 20:49:54 +0200

    zmiany w analysis.py

--- Analysis.py
+++ Analysis.py
@@ -18,7 +18,26 @@ print(data_df.columns)
 print("........")
 print(data_df.info())
 
+
 #"DATA CLEANING"
+
+#funkcja .duplicated() - czy są zduplikowane wiersze
+print(data_df.duplicated())
+# brak zduplikowanych danych
+
+print("........")
 
 #funkcja .isna() - pokaż braki
 print(data_df.isna())
+#brak Nan-ów
+
+#wyświetlanie szumów za pomocą wykresu
+import matplotlib.pyplot as plt
+plt.scatter(data_df.index, data_df['smoker'])
+plt.show()
+# brak szumów
+
+
+print(data_df['smoker'].unique())
+#Out: ['yes' 'no']
+# Brak wartości błędnych - yes/no - prawidłowe odpowiedzi
