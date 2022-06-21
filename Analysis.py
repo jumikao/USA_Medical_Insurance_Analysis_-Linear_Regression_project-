90d8419a7cd1afaf12607ede4876b361b2f19467
jumikao <mezykowska.ma@gmail.com>
2022-06-21 20:39:50 +0200

    zmiany

--- Analysis.py
+++ Analysis.py
@@ -1,3 +1,11 @@
+#DESCRIPTION
+# The aim of this project is to analyse the medical insurance costs of insurance packages in USA
+# and try to predict the potencial medical insurance costs depending on the assumptions given.
+
+
+
+#"DATA COLLECTION"
+
 import pandas as pd
 
 data_df = pd.read_csv(r'https://raw.githubusercontent.com/Kamil128/ProjektPraktycznyRegresja/main/data/medical_cost/medical_cost.csv',
@@ -6,3 +14,11 @@ data_df = pd.read_csv(r'https://raw.githubusercontent.com/Kamil128/ProjektPrakty
 print(data_df.head())
 print("........")
 print(data_df.columns)
+
+print("........")
+print(data_df.info())
+
+#"DATA CLEANING"
+
+#funkcja .isna() - pokaż braki
+print(data_df.isna())
